35dc29132e79839aba245e0e3a9fb17150c43b5b
sandeep1555 <sandeepdasari614@gmail.com>
2024-02-06 19:27:06 +0530

    second commit:

--- indec.js
+++ indec.js
@@ -1,8 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent=React.createElement("div",{id:"parent"},
 [React.createElement("div",{id:"child1"},
-[React.createElement("h1",{id:"haading1"},"this is h1 tag"),React.createElement("h2",{id:"haading"},"this is h2 tag")]),
+[React.createElement("h1",{id:"heading1"},"this is h1 tag"),React.createElement("h2",{id:"heading"},"this is h2 tag")]),
 React.createElement("div",{id:"child1"},
-[React.createElement("h1",{id:"haading2"},"this is h1 tag"),React.createElement("h2",{id:"haading"},"this is h2 tag")])]
+[React.createElement("h1",{id:"heading2"},"this is h1 tag"),React.createElement("h2",{id:"heading"},"this is h2 tag")])]
 
 
 );
